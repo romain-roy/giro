3bb9cf678df64fced825e1b2447541eb3897b80e
Romain Roy <rroy.romain@gmail.com>
2019-11-20 17:05:57 +0100

    Add Timeline

---
+++ Assets/Scripts/Timeline.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timeline : MonoBehaviour
+{
+    public Button valider;
+
+    private Slider timeline;
+    private bool fillTimeline = false;
+
+    void Start()
+    {
+        valider.onClick.AddListener(FillTimeline);
+
+        timeline = gameObject.GetComponent<Slider>();
+        timeline.interactable = false;
+    }
+
+    void Update()
+    {
+        if (fillTimeline && timeline.value < 1)
+            timeline.value += 0.01f;
+    }
+
+    void FillTimeline()
+    {
+        fillTimeline = true;
+    }
+}
