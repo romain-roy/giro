05bf10f7c617870ab4860068c1575b34439db569
Anis HADJARI <anis_sg@hotmail.com>
2019-11-28 15:01:17 +0100

    - Player System

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jumpForce;
-    public float speed;
+    public float moveForce;
 
     private Rigidbody2D rigidBody;
     private Transform playerTransform;
+
+    private Vector2 force = Vector2.zero;
 
     void Start()
     {
@@ -21,28 +23,41 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) this.Jump();
         if (Input.GetKeyDown(KeyCode.RightArrow)) this.Right();
         if (Input.GetKeyDown(KeyCode.LeftArrow)) this.Left();
+        if (Input.GetKeyDown(KeyCode.W)) StartCoroutine(Wait());
+        if (Input.GetKeyDown(KeyCode.A)) Stop();
+
+    }
+
+    void Stop()
+    {
+        rigidBody.velocity = Vector2.zero;
     }
 
     void Jump()
     {
-        Debug.Log("Jump");
-        rigidBody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
     void Right()
     {
-        Debug.Log("Right");
-        playerTransform.position += transform.right * speed * Time.deltaTime;
+        force = Vector2.right * moveForce;
+        rigidBody.velocity = Vector2.zero;
+        rigidBody.AddForce(force, ForceMode2D.Impulse);
     }
 
     void Left()
     {
-        Debug.Log("Left");
-        playerTransform.position += -transform.right * speed * Time.deltaTime;
+        force = Vector2.left * moveForce;
+        rigidBody.velocity = Vector2.zero;
+        rigidBody.AddForce(force, ForceMode2D.Impulse);
     }
 
-    void Wait()
+    IEnumerator Wait()
     {
-        Debug.Log("Wait");
+        Vector2 f = force;
+        rigidBody.velocity = Vector2.zero;
+        yield return new WaitForSeconds(1.0f);
+        force = f;
+        rigidBody.AddForce(force, ForceMode2D.Impulse);
     }
 }
